e7f5c240fab05422589cf3f0e98751bfd5532aff
Stefan Ferstl <st.ferstl@gmail.com>
2015-02-05 19:01:09 +0100

    Handle spaces in path properly
    The script did not work if the Java installation directory
    contains spaces.

--- jdk-verifier.sh
+++ jdk-verifier.sh
@@ -2,37 +2,38 @@
 
 SCRIPT_NAME=`basename $0`
 
+
 if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
   echo "Usage: $SCRIPT_NAME JAVA_HOME" >&2
   exit 1
 fi
 
-if [ ! -f $1/bin/java ]; then
+if [ ! -f "$1"/bin/java ]; then
   echo "$1/bin/java not found $1 is not a JDK installation folder" >&2
   exit 1
 fi
 
-if [ ! -f $1/jre/lib/security/java.security ]; then
+if [ ! -f "$1"/jre/lib/security/java.security ]; then
   echo "$1/jre/lib/security/java.security not found $1 is not a JDK 8 installation folder" >&2
   exit 1
 fi
 
-if [ ! -f $1/jre/lib/security/local_policy.jar ]; then
+if [ ! -f "$1"/jre/lib/security/local_policy.jar ]; then
   echo "$1/jre/lib/security/local_policy.jar not found $1 is not a JDK 8 installation folder" >&2
   exit 1
 fi
 
 
 
-$1/bin/java -version
+"$1"/bin/java -version
 
-if [ -f $1/bin/javadoc ]; then
+if [ -f "$1"/bin/javadoc ]; then
   echo "full JDK"
 else
   echo "server JRE"
 fi
 
-grep "^securerandom\\.source" $1/jre/lib/security/java.security
+grep "^securerandom\\.source" "$1"/jre/lib/security/java.security
 
-unzip -qc $1/jre/lib/security/local_policy.jar default_local.policy
+unzip -qc "$1"/jre/lib/security/local_policy.jar default_local.policy
 
