d750679752d1681cae6f3a406d62045583199288
Stefan Ferstl <st.ferstl@gmail.com>
2015-02-05 20:10:54 +0100

    Improve detection of JRE, server JRE and JDK
    Aditionally, make it work on plain JREs

--- jdk-verifier.sh
+++ jdk-verifier.sh
@@ -2,8 +2,10 @@
 
 SCRIPT_NAME=`basename $0`
 JAVA_EXECUTABLE='bin/java'
-SECURITY_PROPERTIES='jre/lib/security/java.security'
-LOCAL_POLICY='jre/lib/security/local_policy.jar'
+JCMD_EXECUTABLE='bin/jcmd'
+JCONSOLE_EXECUTABLE='bin/jconsole'
+SECURITY_PROPERTIES='lib/security/java.security'
+LOCAL_POLICY='lib/security/local_policy.jar'
 
 if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
   echo "Usage: $SCRIPT_NAME JAVA_HOME" >&2
@@ -15,13 +17,13 @@ if [ ! -f "$1/$JAVA_EXECUTABLE" ]; then
   exit 1
 fi
 
-if [ ! -f "$1/$SECURITY_PROPERTIES" ]; then
-  echo "$SECURITY_PROPERTIES not found. $1 is not a Java 8 installation folder" >&2
+if [ ! -f "$1/$SECURITY_PROPERTIES" ] && [ ! -f "$1/jre/$SECURITY_PROPERTIES" ]; then
+  echo "[jre/]$SECURITY_PROPERTIES not found. $1 is not a Java 8 installation folder" >&2
   exit 1
 fi
 
-if [ ! -f "$1/$LOCAL_POLICY" ]; then
-  echo "$LOCAL_POLICY not found. $1 is not a JDK 8 installation folder" >&2
+if [ ! -f "$1/$LOCAL_POLICY" ] && [ ! -f "$1/jre/$LOCAL_POLICY" ]; then
+  echo "[jre]/$LOCAL_POLICY not found. $1 is not a JDK 8 installation folder" >&2
   exit 1
 fi
 
@@ -29,13 +31,19 @@ fi
 
 "$1/$JAVA_EXECUTABLE" -version
 
-if [ -f "$1"/bin/javadoc ]; then
-  echo "full JDK"
-else
+if [ ! -f "$1/$JCMD_EXECUTABLE" ]; then
+  echo "JRE"
+elif [ ! -f "$1/$JCONSOLE_EXECUTABLE" ]; then
   echo "server JRE"
+else
+  echo "full JDK"
 fi
 
 grep "^securerandom\\.source" "$1"/jre/lib/security/java.security
 
-unzip -qc "$1"/jre/lib/security/local_policy.jar default_local.policy
+if [ -d "$1/jre" ]; then
+  unzip -qc "$1"/jre/lib/security/local_policy.jar default_local.policy
+else
+  unzip -qc "$1"/lib/security/local_policy.jar default_local.policy
+fi
 
